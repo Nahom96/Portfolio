f1fd4c0b43eab4fdebec62ced295a06935f02f5b
Nahom96 <arayanahom475@gmail.com>
2023-09-29 03:00:14 -0400

    first commit

---
+++ app.js
@@ -0,0 +1,32 @@
+// Imports
+const express = require('express')
+const app = express()
+const port = 8081
+
+// Set Views
+app.set('views', './views')
+app.set('view engine', 'ejs')
+
+app.get('', (req, res) => {
+    res.render('index', {text: 'This is S'})
+})
+
+app.get('/about', (req, res) => {
+    res.render('about', {text: 'About Page'})
+})
+
+app.get('/project', (req, res) => {
+    res.render('project', {text: 'This is S'})
+})
+
+app.get('/services', (req, res) => {
+    res.render('services', {text: 'About Page'})
+})
+app.get('/contact', (req, res) => {
+    res.render('contact', {text: 'This is S'})
+})
+
+
+// Listen on port 3000
+app.listen(port, () => console.info(`Listening on port ${port}`))
+

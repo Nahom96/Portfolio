6c7e0130ed36e7add170649f340e4c882f19359e
Nahom96 <arayanahom475@gmail.com>
2023-10-02 19:17:42 -0400

    Comments Added

--- app.js
+++ app.js
@@ -1,3 +1,4 @@
+// app.js file | Name: Nahom Araya, ID: 301180666, Date: Oct 2 2023
 // Imports
 const express = require('express')
 const app = express()
@@ -9,26 +10,27 @@ app.set('views', './views')
 app.set('view engine', 'ejs')
 
 // Statics
-
 app.use(express.static(__dirname + '/public'));
 
 
 // Routes for views
+// route for index
 app.get('', (req, res) => {
     res.render('index')
 })
-
+// route for about
 app.get('/about', (req, res, next) => {
     res.render('about', {text: 'About Page'})
 })
-
+// route for project
 app.get('/project', (req, res, next) => {
     res.render('project', {text: 'This is S'})
 })
-
+// route for services
 app.get('/services', (req, res, next) => {
     res.render('services', {text: 'About Page'})
 })
+// route for contact
 app.get('/contact', (req, res, next) => {
     res.render('contact', {text: 'This is S'})
 })
